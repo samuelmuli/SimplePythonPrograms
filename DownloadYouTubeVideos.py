d9e1a0e280f69a12d37eb8870240ac231a3c2167
samuelmuli <samuelmuli045@gmail.com>
2022-07-24 00:41:34 +0300

    Update DownloadYouTubeVideos.py

--- DownloadYouTubeVideos.py
+++ DownloadYouTubeVideos.py
@@ -1,4 +1,4 @@
 import pytube
 url =input( "Enter URL VIDEO FORMAT: ")
-path = "D:"
+path = "D:" #specify you local path, could be C,D,E etc
 pytube.YouTube(url).streams.get_highest_resolution().download(path)
